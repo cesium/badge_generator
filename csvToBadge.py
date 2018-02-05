1b12667fe9ef967437a58592eaa7c498d082fbc5
resende <resende@red.red.lan>
2018-02-05 20:21:20 +0000

    finished script

---
+++ csvToBadge.py
@@ -0,0 +1,15 @@
+import sys
+import csv
+import subprocess
+
+def geraPDF(badge,numero):
+    subprocess.call(['./heroku.sh '+ badge+' '+numero], shell = True)
+    subprocess.call(['./script.sh '+ badge], shell = True)
+
+file = open(sys.argv[1],'r')
+reader = csv.reader(file)
+for row in reader:
+    geraPDF(row[0],row[1])
+
+
+
--- script.sh
+++ script.sh
@@ -1,13 +1,15 @@
-
-function geraPDF(){
-  heroku run rake sei:nr_qrc_generator\[$1,$2,SU\] > $1.in
+#!/bin/bash
+#function geraPDF(){
+ # heroku run rake sei:qrc_generator\[$1,$2,SU\]
   tail -n +8 $1.in | sed  's///g' > temp
   cat temp > $1.in
-  make ./pdf/$1.pdf
-}
+  make $1.pdf
+  mv $1.pdf pdf
+#}
 
 
-while IFS=, read -r col1 col2
-do
-  geraPDF $col1 $col2
-done < $1
+#while IFS="," read -r col1 col2
+#do
+#  geraPDF $col1 $col2
+#done < $1
+
