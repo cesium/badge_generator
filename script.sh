bd95b0c477c364ed6be5654fe30789efd0411593
resende <resende@red.red.lan>
2018-02-05 16:24:33 +0000

    csv

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ function geraPDF(){
   heroku run rake sei:nr_qrc_generator\[$1,$2,SU\] > $1.in
   tail -n +8 $1.in | sed  's///g' > temp
   cat temp > $1.in
-  make $1.pdf
+  make ./pdf/$1.pdf
 }
 
 
