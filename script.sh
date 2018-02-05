392c9ebffdf6fdf9a9f0835d5c38b96736622945
resende <resende@red.red.lan>
2018-02-05 15:31:27 +0000

    csv parser

--- script.sh
+++ script.sh
@@ -1,4 +1,13 @@
-heroku run rake sei:nr_qrc_generator\[$1,$2,SU\] > $1.in
-tail -n +8 $1.in | sed  's///g' > temp
-cat temp > $1.in
-make $1.pdf
+
+function geraPDF(){
+  heroku run rake sei:nr_qrc_generator\[$1,$2,SU\] > $1.in
+  tail -n +8 $1.in | sed  's///g' > temp
+  cat temp > $1.in
+  make $1.pdf
+}
+
+
+while IFS=, read -r col1 col2
+do
+  geraPDF $col1 $col2
+done < $1
